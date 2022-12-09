9fea859775c5bf72bd50b11b7d796d8585b0b5c1
Kai Takami <kaitakami@protonmail.com>
2022-12-09 21:33:28 +0900

    Solve fizzbuzz with TDD

---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,42 @@
+import { describe, expect, it } from 'vitest'
+import { fizzbuzz } from '../src/fizzbuzz'
+
+describe('fizzbuzz', () => {
+  // it('should be a function', () => {
+  //   expect(typeof fizzbuzz).toBe('function')
+  // })
+
+  it('should throw if not number is provided as paramenter', () => {
+    expect(() => fizzbuzz()).toThrow()
+  })
+
+  it('should throw a specific error message if not number is provided as parameter', () => {
+    expect(() => fizzbuzz(NaN)).toThrow('parameter provided must be a number')
+  })
+
+  it('should return 1 if number provided is 1', () => {
+    expect(fizzbuzz(1)).toBe(1)
+  })
+
+  it('should return 2 if number provided is 2', () => {
+    expect(fizzbuzz(2)).toBe(2)
+  })
+
+  it('should return "fizz" if number provided is multiple of 3 and not multiple of 5', () => {
+    expect(fizzbuzz(3)).toBe('fizz')
+    expect(fizzbuzz(6)).toBe('fizz')
+    expect(fizzbuzz(9)).toBe('fizz')
+  })
+
+  it('should return "buzz" if number provided is multiple of 5 and not multiple of 3', () => {
+    expect(fizzbuzz(5)).toBe('buzz')
+    expect(fizzbuzz(10)).toBe('buzz')
+    expect(fizzbuzz(20)).toBe('buzz')
+  })
+
+  it('should return "fizzbuzz" if number provided is multiple of 3 and 5', () => {
+    expect(fizzbuzz(15)).toBe('fizzbuzz')
+    expect(fizzbuzz(30)).toBe('fizzbuzz')
+    expect(fizzbuzz(45)).toBe('fizzbuzz')
+  })
+})
